38aa65794b85376e42a6199715ee0698844c1265
mohamed-nabil22 <nmohamednabil5@gmail.com>
2023-03-28 12:10:10 -0700

    add project initial commit, demo version

---
+++ Core/Inc/app_config.h
@@ -0,0 +1,14 @@
+/*
+ * app_config.h
+ *
+ *  Created on: Mar 28, 2023
+ *      Author: Mohamed Nabil
+ */
+
+#ifndef INC_APP_CONFIG_H_
+#define INC_APP_CONFIG_H_
+
+#define MAX_SPEED_MEASURE_TIME (uint32_t)1000
+
+
+#endif /* INC_APP_CONFIG_H_ */
